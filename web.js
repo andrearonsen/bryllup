df73a4ded7d9517dfe4f835f4955c05281bd96eb
André Kvist Aronsen <andre.aronsen@gmail.com>
2013-02-02 23:00:27 +0100

    Legger til bootstrap med cosmo.

--- web.js
+++ web.js
@@ -4,17 +4,18 @@ var app = express.createServer(express.logger());
 
 var mongo = require('mongodb');
 
-var mongoUri = process.env.MONGOLAB_URI; 
+var mongoUri = process.env.MONGOLAB_URI || "mongodb://heroku_app11537877:1juhi6cu74fau867pjtaab3vna@ds049467.mongolab.com:49467/heroku_app11537877"; 
 
-mongo.Db.connect(mongoUri, function (err, db) {
+
+
+app.get('/', function(request, response) {
+  mongo.Db.connect(mongoUri, function (err, db) {
   db.collection('gjester', function(er, collection) {
-    collection.insert({'navn1': 'André'}, {safe: true}, function(er,rs) {
+    collection.get({'invitasjonsnummer': 'FAR123'}, {safe: true}, function(er, rs) {
+      response.send('Hello World!' + rs);
     });
   });
 });
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
