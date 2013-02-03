b2ab4abeb1b1f5b3ad4ccf0dea874377db497797
André Kvist Aronsen <andre.aronsen@gmail.com>
2013-02-03 20:33:06 +0100

    Endret til kiwi-template.

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.configure(function() {
   app.use(express.static(__dirname + '/public'));
@@ -13,14 +13,14 @@ var mongo = require('mongodb');
 var mongoUri = process.env.MONGOLAB_URI || "mongodb://heroku_app11537877:1juhi6cu74fau867pjtaab3vna@ds049467.mongolab.com:49467/heroku_app11537877"; 
 
 app.get('/', function(request, response) {
-  mongo.Db.connect(mongoUri, function (err, db) {
-  db.collection('gjester', function(er, collection) {
-    collection.get({'invitasjonsnummer': 'FAR123'}, {safe: true}, function(er, rs) {
-      //response.sendFile('public/index.html');
-      response.render("index", {});
-    });
-  });
-});
+  response.render("index", {gjester : "hei sveis"});
+  // mongo.Db.connect(mongoUri, function (err, db) {
+  // db.collection('gjester', function(er, collection) {
+  //   collection.get({'invitasjonsnummer': 'FAR123'}, {safe: true}, function(er, rs) {
+  //     response.render("index.kiwi", {gjester : rs});
+  //   });
+  // });
+// });
 });
 
 var port = process.env.PORT || 5000;
