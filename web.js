40ba6967a4b7bcd0b1fbe69d2a79600c445ca7b5
André Kvist Aronsen <andre.aronsen@gmail.com>
2013-02-03 21:41:41 +0100

    Prøvd ut forskjellige template engines, ender opp med Handlebars?

--- web.js
+++ web.js
@@ -1,27 +1,47 @@
-var express = require('express');
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
 
 var app = express();
 
 app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
+  app.set('port', process.env.PORT || 5000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'hbs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.set('view engine', 'kiwi');
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
 
-var mongo = require('mongodb');
+app.get('/', routes.index);
+  
+// app.get('/', routes.index);
+// app.get('/users', user.list);
 
-var mongoUri = process.env.MONGOLAB_URI || "mongodb://heroku_app11537877:1juhi6cu74fau867pjtaab3vna@ds049467.mongolab.com:49467/heroku_app11537877"; 
+// var mongo = require('mongodb');
 
-app.get('/', function(request, response) {
-  response.render("index", {gjester : "hei sveis"});
-  // mongo.Db.connect(mongoUri, function (err, db) {
-  // db.collection('gjester', function(er, collection) {
-  //   collection.get({'invitasjonsnummer': 'FAR123'}, {safe: true}, function(er, rs) {
-  //     response.render("index.kiwi", {gjester : rs});
-  //   });
-  // });
+// var mongoUri = process.env.MONGOLAB_URI || "mongodb://heroku_app11537877:1juhi6cu74fau867pjtaab3vna@ds049467.mongolab.com:49467/heroku_app11537877"; 
+
+// app.get('/', function(req, res) {
+//   res.render("test", {title : "Test title", name:"Andre"});
+//   // mongo.Db.connect(mongoUri, function (err, db) {
+//   // db.collection('gjester', function(er, collection) {
+//   //   collection.get({'invitasjonsnummer': 'FAR123'}, {safe: true}, function(er, rs) {
+//   //     response.render("index.kiwi", {gjester : rs});
+//   //   });
+//   // });
+// // });
 // });
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
