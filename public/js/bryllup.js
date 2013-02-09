1c5b0f05a0b447bae50d3160a11347e5f6ad948c
André Kvist Aronsen <andre.aronsen@gmail.com>
2013-02-09 17:56:00 +0100

    Bruk jade og lagt til logikk.

--- public/js/bryllup.js
+++ public/js/bryllup.js
@@ -1,8 +1,61 @@
 var BRYLLUP = this.BRYLLUP || {};
 
-(function (win, $) {
+(function (win, $, _, B) {
   "use strict";
 
+  var meldingTemplate = '<div id="melding" class="alert">{{tekst}}</div>';
+  var fantIkkeKodeTekst = 'Fant dessverre ikke invitasjonskoden.';
+  var serverFeilTekst = 'Det har oppstått en feil på serveren, beklager!';
+
+  B.meldinger = {};
+
+  function konfigurer() {
+    _.templateSettings = {
+      interpolate : /\{\{(.+?)\}\}/g
+    };
+
+    var meldingContainer = $('#meldingContainer');
+    var fantIkkeKodeMelding = $(_.template(meldingTemplate, {tekst: fantIkkeKodeTekst}));
+    var serverFeilMelding = $(_.template(meldingTemplate, {tekst: serverFeilTekst}));
+    
+    B.meldinger.fantIkkeKode = function () {
+      meldingContainer.empty();
+      fantIkkeKodeMelding.appendTo(meldingContainer);
+    };
+    B.meldinger.serverFeil = function () {
+      meldingContainer.empty();
+      serverFeilMelding.appendTo(meldingContainer);
+    };
+
+    B.invitasjonskodeInput = $("#invitasjonskodeInput");
+    B.fokusInvitasjonskodeInput = function () {
+      B.invitasjonskodeInput.focus();
+    };
+    B.hentVerdiInvitasjonskodeInput = function () {
+      return B.invitasjonskodeInput.get(0).value;
+    };
+  }
+
+  function forwardTilHovedside(invitasjonskode) {
+    win.location = "/hovedside/" + invitasjonskode;  
+  }
+
+  function forwardTilIndex() {
+    win.location = "/";  
+  }
+
+  function hentLagretInvitasjonskode() {
+    return win.localStorage["invitasjonskode"];
+  }
+
+  function lagreInvitasjonskode(invitasjonskode) {
+    win.localStorage["invitasjonskode"] = invitasjonskode;
+  }
+
+  function fjernInvitasjonskode() {
+    win.localStorage.removeItem("invitasjonskode");
+  }
+  
   function sjekkInvitasjonskode(invitasjonskode) {
     $.ajax({
       type: 'GET',
@@ -10,37 +63,52 @@ var BRYLLUP = this.BRYLLUP || {};
       url: "/sjekkinvitasjonskode/" + invitasjonskode,
       statusCode: {
         200: function () {
-          $("#melding").text("");  
+          
         },
         404: function () {
-          $("#melding").text("Invitasjonskoden finnes ikke.");
-          $("#invitasjonskodeInput").focus();
+          B.fokusInvitasjonskodeInput();
+          B.meldinger.fantIkkeKode();
         },
         500: function () {
-          $("#melding").text("Feil på server.");
+          B.fokusInvitasjonskodeInput();
+          B.meldinger.serverFeil();
         }
       }
     }).done(function (item) {
-      alert('OK: ' + JSON.stringify(item));
-      win.location = "/hovedside/" + invitasjonskode;
-    });
+      lagreInvitasjonskode(invitasjonskode);
+      forwardTilHovedside(invitasjonskode);
+    });   
   }
 
-  BRYLLUP.sjekkInvitasjonskode = sjekkInvitasjonskode;
+  function startIndex() {
+    konfigurer();
 
-  BRYLLUP.start = function () {
+    var lagretInvitasjonskode = hentLagretInvitasjonskode();
+    if (lagretInvitasjonskode) {
+      forwardTilHovedside(lagretInvitasjonskode);
+      return;
+    }
+
     $("#sjekkInvitasjonskodeKnapp").click(function (e) {
-      var invitasjonskode = $("#invitasjonskodeInput").get(0).value;
-      sjekkInvitasjonskode(invitasjonskode);  
+      sjekkInvitasjonskode(B.hentVerdiInvitasjonskodeInput());  
     });
 
-    $("#invitasjonskodeInput").keyup(function (e) {
+    B.invitasjonskodeInput.keyup(function (e) {
       if (e.which == 13) {
         var invitasjonskode = $(this).get(0).value;
         sjekkInvitasjonskode(invitasjonskode);
       }   
     }).focus();
-  };
-}(window, jQuery));
+  }
 
-BRYLLUP.start();
+  function startHovedside() {
+    $("#loggut").click(function (e) {
+      fjernInvitasjonskode();  
+    });
+  }
+
+  B.startIndex = startIndex;
+  B.startHovedside = startHovedside;
+  B.fjernInvitasjonskode = fjernInvitasjonskode;
+  B.forwardTilIndex = forwardTilIndex;
+}(window, jQuery, _, BRYLLUP));
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var mongo = require('mongodb');
+var fn = require('underscore');
+var fn_s = require('underscore.string');
 
-// var bryllupDbUri = process.env.MONGOLAB_URI || "mongodb://localhost:27017/bryllup";
+var bryllupDbUri_local = "mongodb://localhost:27017/bryllup";
 var bryllupDbUri = "mongodb://nodejitsu_andrearonsen:5kn803s0rp2nrpt3r4cplahs7u@ds049537.mongolab.com:49537/nodejitsu_andrearonsen_nodejitsudb7379247420";
 
 function hentInvitasjon(invitasjonskode, behandle_invitasjon) {
@@ -10,6 +12,15 @@ function hentInvitasjon(invitasjonskode, behandle_invitasjon) {
       collection.findOne({'invitasjonskode': invitasjonskode}, behandle_invitasjon);  
     });
   });
+}
+
+function berikInvitasjon(invitasjon) {
+  invitasjon.tiltale = function () {
+    var fornavn = function (gjest) {return gjest.fornavn};
+    var gjester = fn.compact([this.gjest1, this.gjest2, this.gjest3]).map(fornavn);
+    console.log('Gjester: ' + gjester[0]);
+    return fn_s.toSentence(gjester, ", ", " og ");
+  };
 }
 
 exports.index = function(req, res) {
@@ -43,7 +54,8 @@ exports.hovedside = function(req, res) {
       res.status(500).send('Klarte ikke å hente ut invitasjonen dessverre, prøv igjen senere!');  
     } else {
       console.log("Result: " + JSON.stringify(item));
-      res.render("hovedside", {title: "Bryllup 24. august 2013", gjester : item}); 
+      berikInvitasjon(item);
+      res.render("hovedside", {title: "Bryllup 24. august 2013", invitasjon : item}); 
     }
   });
 };
