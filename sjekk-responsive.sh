77ec269b88b636b5cfd491d8b8284bf95ab87afc
André Kvist Aronsen <andre.aronsen@gmail.com>
2013-02-17 14:27:10 +0100

    Fikset første versjon av index-side for mobile.

---
+++ sjekk-responsive.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+review --sites='{"Bryllup-index":"http://bryllup.fagerliearonsen.com", "Bryllup-Hoved":"http://bryllup.fagerliearonsen.com/hovedside/hymenslenker"}' --resolutions='["1600x1200", "768x1024", "320×568", "320x480", "320x356"]'
